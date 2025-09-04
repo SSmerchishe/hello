d134ae57e7be2295e448e7611379b0d792851a69
ssmerchishe <smerchishe@gmail.com>
2025-09-04 21:29:11 +0000

    edit cmakelists.txt

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("V obshem. Ya podcluchilsya cherez VSCode po SSH k svoei VW.\n Chtoby delat' izmeneniya s localnoy mashiny.\n Zachem? ya XZ\n");
+    printf("Vvvvv obshem. Ya podcluchilsya cherez VSCode po SSH k svoei VW.\n Chtoby delat' izmeneniya s localnoy mashiny.\n Zachem? ya XZ\n");
     return 0;
 }
