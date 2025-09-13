f003af65cf3b82e7cc0739eabffcab635bf7dd27
Fogotcheck <fogotcheck@live.com>
2025-09-13 22:46:15 +0300

    fix: add clang-format. Closes #3

--- hello.c
+++ hello.c
@@ -2,26 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-void key_output(int count_argc, char **argv, char *key, char *print_message) {
-    if (strcmp(argv[1], key) == 0) {
-        printf("%s\n", print_message);
-    }
+void key_output(int     count_argc, 
+                char    **argv, 
+                char    *key, 
+                char    *print_message)
+{
+	if (strcmp(argv[1], key) == 0) {
+		printf("%s\n", print_message);
+	}
 }
 
-int main(int argc, char *argv[]) {
-    int res = 0;
-    if (argc > 1) {
-        key_output(argc, argv, "--version", PROJECT_VERSION);
-        key_output(argc, argv, "--githash", GIT_HASH);
-        key_output(argc, argv, "--help", "Created by SSmerchishe.\
-            \nAvailable keys:\n\t--help - Help;\
-            \n\t--version - Version.\
-            \n\t--githash - Git Hash.\n");
-        return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
-    }
+int main(int argc, char *argv[])
+{
+	int res = 0;
 
-    printf("hello\n");
-    return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+	if (argc > 1) {
+		key_output(argc, argv,  "--version", PROJECT_VERSION);
+		key_output(argc, argv,  "--githash", GIT_HASH);
+		key_output(argc, argv,  "--help", 
+                                "Created by SSmerchishe.       \
+                                \nAvailable keys:              \
+                                    \n\t--help\t\t- Help;      \
+                                    \n\t--version\t- Version.  \
+                                    \n\t--githash\t- Git Hash. \n");
+
+		return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+	}
+
+	printf("hello\n");
+
+	return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
