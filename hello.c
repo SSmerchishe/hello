4bd7ed84e209f5f3d91a88579aef73ca9f043e60
SSmerchishe <smerchishe@gmail.com>
2025-09-13 17:56:03 +0000

    Add stdlib.h

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 
 void key_output(int count_argc, char **argv, char *key, char *print_message) {
     if (strcmp(argv[1], key) == 0) {
@@ -8,16 +10,17 @@ void key_output(int count_argc, char **argv, char *key, char *print_message) {
 }
 
 int main(int argc, char *argv[]) {
+    int res = 0;
     if (argc > 1) {
         key_output(argc, argv, "--version", PROJECT_VERSION);
         key_output(argc, argv, "--githash", GIT_HASH);
         key_output(argc, argv, "--help", "Created by SSmerchishe.\
             \nAvailable keys:\n\t--help - Help; \
             \n\t--version - Version.\n");
-        return 0;
+        return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
     }
 
     printf("hello\n");
-    return 0;
+    return res == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
